eef023048dc19e6b0b691feaa7b24d0837264f23
sisizov-dev <sisizov@gmail.com>
2026-06-27 22:31:26 +0100

    add class test_faq.py

---
+++ tests/test_faq.py
@@ -0,0 +1,28 @@
+import pytest
+from pages.main_page import MainPage
+from locators.main_page_locators import MainPageLocators
+
+
+class TestFaq:
+
+    @pytest.mark.parametrize("index, expected_part", [
+        (0, "400 рублей"),
+        (1, "один заказ — один самокат"),
+        (2, "8 мая"),
+        (3, "завтрашнего дня"),
+        (4, "1010"),
+        (5, "восемь суток"),
+        (6, "Штрафа не будет"),
+        (7, "Москве")
+    ])
+    def test_faq_answers(self, driver, index, expected_part):
+        main_page = MainPage(driver, MainPageLocators)
+        
+        main_page.accept_cookies()
+        main_page.scroll_to_faq_block()
+        main_page.click_question(index)
+        
+        assert main_page.is_answer_visible(index), f"Ответ на вопрос {index} не видим"
+        
+        actual_text = main_page.get_answer_text(index)
+        assert expected_part in actual_text, f"'{expected_part}' не найдено в тексте: {actual_text}"
