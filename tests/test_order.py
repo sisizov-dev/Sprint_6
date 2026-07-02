ce4c1f1027fa53ef3251b2a4b9c6d47c467bb676
sisizov-dev <sisizov@gmail.com>
2026-07-02 12:56:23 +0100

    upgrade test_order.py

--- tests/test_order.py
+++ tests/test_order.py
@@ -1,3 +1,4 @@
+
 import pytest
 import allure
 from pages.order_page import OrderPage
@@ -12,63 +13,29 @@ class TestOrder:
     @allure.title("Позитивный сценарий заказа самоката")
     @allure.description("Проверка полного флоу заказа с двумя наборами данных")
     @allure.story("Оформление заказа")
-    @pytest.mark.parametrize("button_position, name, surname, address, station, phone, date, period, color", [
-        ("top", "Олег", "Кузин", "г. Москва, ул. Зорге, д. 5", "Фрунзенская", "+79995554444", "15", "двое суток", "black"),
-        ("bottom", "Ольга", "Михеева", "Московская обл., г. Красногорск, ул. Витте, д. 7", "Митино", "+79998882233", "20", "семеро суток", "grey")
+    @pytest.mark.parametrize("button_method, color_method, name, surname, address, station, phone, date, period", [
+        (OrderPage.click_order_button_top, OrderPage.select_color_black, "Олег", "Кузин", "г. Москва, ул. Зорге, д. 5", "Фрунзенская", "+79995554444", "15", "двое суток"),
+        (OrderPage.click_order_button_bottom, OrderPage.select_color_grey, "Ольга", "Михеева", "Московская обл., г. Красногорск, ул. Витте, д. 7", "Митино", "+79998882233", "20", "семеро суток")
     ])
-    def test_order_flow(self, driver, button_position, name, surname, address, station, phone, date, period, color):
+    def test_order_flow(self, driver, button_method, color_method, name, surname, address, station, phone, date, period):
+        main_page = MainPage(driver, MainPageLocators)
+        main_page.accept_cookies()
+
         order_page = OrderPage(driver, OrderPageLocators)
 
-        @allure.step("Открыть главную страницу и принять куки")
-        def step_accept_cookies():
-            main_page = MainPage(driver, MainPageLocators)
-            main_page.accept_cookies()
+        button_method(order_page)
+        order_page.fill_name(name)
+        order_page.fill_surname(surname)
+        order_page.fill_address(address)
+        order_page.select_metro(station)
+        order_page.fill_phone(phone)
+        order_page.click_next()
 
-        @allure.step(f"Нажать кнопку 'Заказать' ({button_position})")
-        def step_click_order_button():
-            if button_position == "top":
-                order_page.click_order_button_top()
-            else:
-                order_page.click_order_button_bottom()
+        order_page.select_date(date)
+        order_page.select_rental_period(period)
+        color_method(order_page)
 
-        @allure.step("Заполнить первую часть формы")
-        def step_fill_first_form():
-            order_page.fill_name(name)
-            order_page.fill_surname(surname)
-            order_page.fill_address(address)
-            order_page.select_metro(station)
-            order_page.fill_phone(phone)
+        order_page.click_order()
+        order_page.click_yes()
 
-        @allure.step("Нажать 'Далее'")
-        def step_click_next():
-            order_page.click_next()
-
-        @allure.step("Заполнить вторую часть формы")
-        def step_fill_second_form():
-            order_page.select_date(date)
-            order_page.select_rental_period(period)
-            if color == "black":
-                order_page.select_color_black()
-            else:
-                order_page.select_color_grey()
-
-        @allure.step("Нажать 'Заказать'")
-        def step_click_order():
-            order_page.click_order()
-
-        @allure.step("Подтвердить заказ (кнопка 'Да')")
-        def step_click_yes():
-            order_page.click_yes()
-
-        @allure.step("Проверить сообщение об успешном заказе")
-        def step_check_success():
-            assert order_page.is_success_displayed(), "Сообщение об успешном заказе не отображается"
-
-        step_accept_cookies()
-        step_click_order_button()
-        step_fill_first_form()
-        step_click_next()
-        step_fill_second_form()
-        step_click_order()
-        step_click_yes()
-        step_check_success()
+        assert order_page.is_success_displayed(), "Сообщение об успешном заказе не отображается"
