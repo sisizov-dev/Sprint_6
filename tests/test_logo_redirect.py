da578527d0da555dbc0e0207b7a231d5e04fdb0f
sisizov-dev <sisizov@gmail.com>
2026-07-02 12:56:00 +0100

    upgrade test_logo_redirect.py

--- tests/test_logo_redirect.py
+++ tests/test_logo_redirect.py
@@ -2,9 +2,8 @@ from pages.main_page import MainPage
 from pages.order_page import OrderPage
 from locators.main_page_locators import MainPageLocators
 from locators.order_page_locators import OrderPageLocators
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import allure
+
 
 @allure.feature("Переходы по логотипам")
 class TestLogoRedirect:
@@ -15,31 +14,12 @@ class TestLogoRedirect:
     def test_yandex_logo_redirect(self, driver):
         main_page = MainPage(driver, MainPageLocators)
 
-        @allure.step("Принять куки")
-        def step_accept_cookies():
-            main_page.accept_cookies()
+        main_page.accept_cookies()
+        main_page.click_yandex_logo()
+        main_page.switch_to_new_window()
+        main_page.wait_url_contains("dzen.ru")
 
-        @allure.step("Кликнуть по логотипу Яндекса")
-        def step_click_yandex_logo():
-            main_page.click_yandex_logo()
-
-        @allure.step("Переключиться на новую вкладку")
-        def step_switch_to_new_window():
-            driver.switch_to.window(driver.window_handles[1])
-
-        @allure.step("Дождаться загрузки страницы Дзена")
-        def step_wait_for_dzen():
-            WebDriverWait(driver, 10).until(EC.url_contains("dzen.ru"))
-
-        @allure.step("Проверить, что открылся Дзен")
-        def step_check_url():
-            assert "dzen.ru" in driver.current_url
-
-        step_accept_cookies()
-        step_click_yandex_logo()
-        step_switch_to_new_window()
-        step_wait_for_dzen()
-        step_check_url()
+        assert "dzen.ru" in driver.current_url
 
     @allure.title("Переход по логотипу Самоката")
     @allure.description("При клике на логотип Самоката открывается главная страница")
@@ -47,25 +27,10 @@ class TestLogoRedirect:
     def test_scooter_logo_redirect(self, driver):
         main_page = MainPage(driver, MainPageLocators)
 
-        @allure.step("Принять куки")
-        def step_accept_cookies():
-            main_page.accept_cookies()
+        main_page.accept_cookies()
 
-        @allure.step("Открыть страницу заказа")
-        def step_open_order_page():
-            order_page = OrderPage(driver, OrderPageLocators)
-            order_page.click_order_button_top()
-            return order_page
+        order_page = OrderPage(driver, OrderPageLocators)
+        order_page.click_order_button_top()
+        order_page.click_scooter_logo()
 
-        @allure.step("Кликнуть по логотипу Самоката")
-        def step_click_scooter_logo(order_page):
-            order_page.click_scooter_logo()
-
-        @allure.step("Проверить, что открылась главная страница")
-        def step_check_url():
-            assert "qa-scooter.praktikum-services.ru" in driver.current_url
-
-        step_accept_cookies()
-        order_page = step_open_order_page()
-        step_click_scooter_logo(order_page)
-        step_check_url()
+        assert "qa-scooter.praktikum-services.ru" in driver.current_url
