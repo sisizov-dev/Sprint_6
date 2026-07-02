b6a1f3fbc921b2998e2a7e04a01b90824809980a
sisizov-dev <sisizov@gmail.com>
2026-07-02 12:55:12 +0100

    upgrade test_faq.py

--- tests/test_faq.py
+++ tests/test_faq.py
@@ -1,6 +1,3 @@
-import pytest
-import allure
-from pages.main_page import MainPage
 from locators.main_page_locators import MainPageLocators
 
 
@@ -23,29 +20,11 @@ class TestFaq:
     def test_faq_answers(self, driver, index, expected_part):
         main_page = MainPage(driver, MainPageLocators)
 
-        @allure.step("Принять куки")
-        def step_accept_cookies():
-            main_page.accept_cookies()
+        main_page.accept_cookies()
+        main_page.scroll_to_faq_block()
+        main_page.click_question(index)
 
-        @allure.step("Прокрутить до блока 'Вопросы о важном'")
-        def step_scroll_to_faq():
-            main_page.scroll_to_faq_block()
+        assert main_page.is_answer_visible(index), f"Ответ на вопрос {index} не видим"
 
-        @allure.step(f"Кликнуть по вопросу с индексом {index}")
-        def step_click_question():
-            main_page.click_question(index)
-
-        @allure.step(f"Проверить видимость ответа на вопрос {index}")
-        def step_check_visibility():
-            assert main_page.is_answer_visible(index), f"Ответ на вопрос {index} не видим"
-
-        @allure.step(f"Проверить текст ответа на вопрос {index}")
-        def step_check_text():
-            actual_text = main_page.get_answer_text(index)
-            assert expected_part in actual_text, f"'{expected_part}' не найдено в тексте: {actual_text}"
-
-        step_accept_cookies()
-        step_scroll_to_faq()
-        step_click_question()
-        step_check_visibility()
-        step_check_text()
+        actual_text = main_page.get_answer_text(index)
+        assert expected_part in actual_text, f"'{expected_part}' не найдено в тексте: {actual_text}"
